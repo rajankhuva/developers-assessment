a2382d7ce3fb515e356356ef6a1d381f87555f3b
Rajendrakumar khuva <khuva.rajan14@gmail.com>
2026-03-18 22:04:37 +0530

    load dashboard to home (outlet) route

--- frontend/src/routes/_layout/index.tsx
+++ frontend/src/routes/_layout/index.tsx
@@ -1,6 +1,6 @@
-import { createFileRoute } from "@tanstack/react-router"
+import { createFileRoute } from "@tanstack/react-router";
 
-import useAuth from "@/hooks/useAuth"
+import { WorklogDashboard } from "@/components/WorklogDashboard";
 
 export const Route = createFileRoute("/_layout/")({
   component: Dashboard,
@@ -11,21 +11,12 @@ export const Route = createFileRoute("/_layout/")({
       },
     ],
   }),
-})
+});
 
 function Dashboard() {
-  const { user: currentUser } = useAuth()
-
   return (
     <div>
-      <div>
-        <h1 className="text-2xl truncate max-w-sm">
-          Hi, {currentUser?.full_name || currentUser?.email} 👋
-        </h1>
-        <p className="text-muted-foreground">
-          Welcome back, nice to see you again!!!
-        </p>
-      </div>
+      <WorklogDashboard />
     </div>
-  )
+  );
 }
